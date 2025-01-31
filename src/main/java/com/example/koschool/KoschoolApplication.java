c19a72b5918dd441fc338b8cc41a5782d9ad7fae
SL313 <rebin0313@gmail.com>
2025-01-31 15:13:05 +0900

    initial commit

---
+++ src/main/java/com/example/koschool/KoschoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.koschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KoschoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KoschoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/koschool/KoschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.koschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KoschoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
